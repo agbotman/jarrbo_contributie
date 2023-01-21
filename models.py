992a2298465597da0bc962624cd1d55a66897ed9
agbotman <ton@jarrbo.nl>
2023-01-21 18:47:36 +0100

    Import datetime iso date

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import Min, Max, Sum
 import decimal
-from datetime import date
+from datetime import datetime
 from solo.models import SingletonModel
 from .tools import valid_iban
 

f9ddf164795d778003883f12781ae9d096262e77
agbotman <ton@jarrbo.nl>
2023-08-29 16:33:03 +0200

    from django.urls import re_path

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import re_path
+from django.urls import re_path
 from .views import ImportMemberView, ImportMemberDetail, \
                     ImportInschrijvingenView, ImportInschrijvingenDetail, \
                     ImportMachtigingenView, ImportMachtigingenDetail, MemberListView, \

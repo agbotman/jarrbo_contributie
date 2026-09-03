57039f673b041b922e0f339e1fd52d8f3e46ee82
Ton Botman <ton@jarrbo.nl>
2026-09-03 10:22:46 +0200

    roepnaam en achternaam in NotPayedExport

--- views.py
+++ views.py
@@ -507,7 +507,8 @@ def NotPayedExport(request):
     for c in Contribution.seizoen_objects.all():
         betaald = Payment.seizoen_objects.filter(contribution=c,status__status='Betaald').aggregate(total=Sum('amount'))['total'] or 0
         if c.total_contribution > betaald:
-            writer.writerow([c.member.relatiecode, c.member.fullname, c.member.lc, c.payment_method, c.tc, betaald])
+            writer.writerow([c.member.relatiecode, c.member.fullname, c.member.roepnaam, c.member.achternaam, \
+                              c.member.lc, c.payment_method, c.tc, betaald])
     return response
         
 class FactuurView(View):
